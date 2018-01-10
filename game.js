447e970b651799da5c2665da46922d630944f082
Ruha Peera <ruha.rizwan@gmail.com>
2018-01-10 00:04:58 -0500

    asks user to guess a letter using inquirer

---
+++ game.js
@@ -0,0 +1,18 @@
+//this is the main file from which the game  is run
+
+//requiring the inquirer and chalk packages
+var inquirer = require('inquirer');
+var chalk = require('chalk');
+ 
+//prompting the user to select a letter
+inquirer.prompt([
+	{
+	name: "guess",
+	message: " Guess a letter!"
+	},
+	]).then(answers => {
+	//checking if inquirer and chalk packages function
+    // console.log(chalk.cyan(answers.guess + "\n"));
+});
+
+
