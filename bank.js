1ac4042f4d5380805cf82daeadfca54088cf56af
Ruha Peera <ruha.rizwan@gmail.com>
2018-01-10 22:19:05 -0500

    recognizes the corrrect and incorrect letter from random chosen word

--- bank.js
+++ bank.js
@@ -1,11 +1,14 @@
 //this file holds all the words to be used (word bank)
 
-//creating the wordBank object, that holds the wordBankList
-var wordBank = {
+//creating the wordBank array that holds the list of words
+var wordBank = ["mercury", "venus", "earth", "mars", "jupiter"];
 
-	wordBankList: ["mercury", "venus", "earth", "mars", "jupiter"]
+//creating a variable that will choose a random word from the word bank
+var computerChoice = wordBank[Math.floor((Math.random() * wordBank.length))];
 
-};
+//checking if randomization works
+// console.log(computerChoice);
+
 
 //exporting the object so it can be utillized by other files
-module.exports = wordBank;
+module.exports = computerChoice;
